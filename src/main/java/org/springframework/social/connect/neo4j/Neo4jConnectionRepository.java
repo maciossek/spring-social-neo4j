7ba50389d731ef16d96cd808dbddff7ab17ec606
Mathias Maciossek <mathias.maciossek@gmail.com>
2013-05-14 09:10:44 +0200

    -
    Signed-off-by: Mathias Maciossek <mathias.maciossek@gmail.com>

--- src/main/java/org/springframework/social/connect/neo4j/Neo4jConnectionRepository.java
+++ src/main/java/org/springframework/social/connect/neo4j/Neo4jConnectionRepository.java
@@ -1,6 +1,5 @@
 package org.springframework.social.connect.neo4j;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,16 +13,16 @@ import org.apache.log4j.Logger;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.index.IndexManager;
+import org.neo4j.graphdb.Node;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionData;
+import org.springframework.social.connect.ConnectionFactory;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionKey;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.DuplicateConnectionException;
-import org.springframework.social.connect.support.OAuth1Connection;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
@@ -52,34 +51,17 @@ public class Neo4jConnectionRepository implements ConnectionRepository {
 
 	@Override
 	public MultiValueMap<String, Connection<?>> findAllConnections() {
-		log.debug(userId);
-		String shortQuery = "START n=node(1) CREATE (userconnection {userId:'maciossek'})-[:HAS_SOCIAL_CONNECTION]->n RETURN p";
-		ExecutionResult result = engine.execute(shortQuery);
-		log.debug("\n" + result);
-
 		ExecutionResult results = engine.execute("START user=node:User('username:" + userId
 				+ "') MATCH user-[:HAS_SOCIAL_CONNECTION]->connection RETURN connection");
+
+		List<Connection<?>> resultList = createResultList(results);
+
 		MultiValueMap<String, Connection<?>> connections = new LinkedMultiValueMap<String, Connection<?>>();
 		Set<String> registeredProviderIds = connectionFactoryLocator.registeredProviderIds();
+
 		for (String registeredProviderId : registeredProviderIds) {
 			connections.put(registeredProviderId, Collections.<Connection<?>> emptyList());
 		}
-		log.debug("\n" + results);
-
-		List<Connection<?>> resultList = new ArrayList<Connection<?>>();
-
-		for (Iterator<Map<String, Object>> it = results.iterator(); it.hasNext();) {
-			log.debug(it);
-			// resultList.add((Connection<?>) it.next());
-		}
-
-		log.debug(resultList);
-
-		// map resultList to List<Connection<?>>
-		// call iterator() to get a Map<String,Object> of result objects for
-		// each match in the Cypher query
-		// use them to create a Connection object that you add to a
-		// List<Connection<?>>
 
 		for (Connection<?> connection : resultList) {
 			String providerId = connection.getKey().getProviderId();
@@ -88,16 +70,23 @@ public class Neo4jConnectionRepository implements ConnectionRepository {
 			}
 			connections.add(providerId, connection);
 		}
+
 		return connections;
+
 	}
 
 	@Override
 	public List<Connection<?>> findConnections(String providerId) {
+		// return jdbcTemplate.query(selectFromUserConnection() +
+		// " where userId = ? and providerId = ? order by rank",
+		// connectionMapper, userId, providerId);
+		
 		// get Nodes with startnode user with userId and relationship
-		// HAS_SOCIAL_CONNECTIONs
-
-		List<Connection<?>> connections = new ArrayList<Connection<?>>();
-
+		// HAS_SOCIAL_CONNECTION
+		ExecutionResult results = engine.execute("START user=node:User('username:" + userId 
+				+ ", providerId:"+ providerId
+				+ "') MATCH user-[:HAS_SOCIAL_CONNECTION]->connection RETURN connection");
+		List<Connection<?>> connections = createResultList(results);
 		return connections;
 	}
 
@@ -145,11 +134,10 @@ public class Neo4jConnectionRepository implements ConnectionRepository {
 			String query = "START user=node:User(username={userId}) "
 					+ "CREATE (userconnection {providerId:{providerId},providerUserId:{providerUserId}, "
 					+ "displayName:{displayName}, profileUrl:{profileUrl}, imageUrl:{imageUrl}, accessToken:{accessToken}, "
-					+ "secret:{secret}, refreshToken:{refreshToken}, expireTime:{expireTime}})-[:xHAS_SOCIAL_CONNECTION]->user ";
-					//+ "RETURN connection";
-			ExecutionResult result = engine.execute(query, mapUserConnection(connection));
+					+ "secret:{secret}, refreshToken:{refreshToken}, expireTime:{expireTime}})<-[:HAS_SOCIAL_CONNECTION]-user ";
+			// + "RETURN connection";
+			ExecutionResult result = engine.execute(query, mapUserConnectionParams(connection));
 			log.debug(result);
-
 		} catch (DuplicateKeyException e) {
 			throw new DuplicateConnectionException(connection.getKey());
 		}
@@ -181,11 +169,10 @@ public class Neo4jConnectionRepository implements ConnectionRepository {
 		return text != null ? textEncryptor.encrypt(text) : text;
 	}
 
-	private HashMap<String, Object> mapUserConnection(Connection<?> connection) {
+	private HashMap<String, Object> mapUserConnectionParams(Connection<?> connection) {
 		ConnectionData data = connection.createData();
 		Map<String, Object> params = new HashMap<String, Object>();
-		
-		
+
 		params.put("userId", userId);
 		params.put("providerId", data.getProviderId());
 		params.put("providerUserId", data.getProviderUserId());
@@ -196,16 +183,77 @@ public class Neo4jConnectionRepository implements ConnectionRepository {
 		params.put("secret", encrypt(data.getSecret()));
 		params.put("refreshToken", encrypt(data.getRefreshToken()));
 		params.put("expireTime", data.getExpireTime());
-		
-		
+
 		// remove null entries, to work with Neo4j
-		for(Entry<String, Object> entry : params.entrySet()){
-			if(entry.getValue() == null){
+		for (Entry<String, Object> entry : params.entrySet()) {
+			if (entry.getValue() == null) {
 				entry.setValue("");
 			}
 		}
 
 		return (HashMap<String, Object>) params;
+	}
+
+	private String decrypt(String encryptedText) {
+		return encryptedText != null ? textEncryptor.decrypt(encryptedText) : encryptedText;
+	}
+
+	private Long expireTime(long expireTime) {
+		return expireTime == 0 ? null : expireTime;
+	}
+
+//	private List<Connection<?>> mapResultList(ExecutionResult results) {
+//		// create resultList with Connections & convert Cypher query
+//		List<Connection<?>> resultList = new LinkedList<Connection<?>>();
+//
+//		// NumberFormatException
+//		Iterator<Node> userConnections = results.columnAs("connection");
+//		while (userConnections.hasNext()) {
+//			Node uc = userConnections.next();
+//			ConnectionData connectionData = new ConnectionData(
+//					(String) uc.getProperty("providerId"),
+//					(String) uc.getProperty("providerUserId"),
+//					(String) uc.getProperty("displayName"), 
+//					(String) uc.getProperty("profileUrl"),
+//					(String) uc.getProperty("imageUrl"),
+//					decrypt((String) uc.getProperty("accessToken")),
+//					decrypt((String) uc.getProperty("secret")),
+//					decrypt((String) uc.getProperty("refreshToken")),
+//					expireTime((Long) uc.getProperty("expireTime")));
+//			ConnectionFactory<?> connectionFactory = connectionFactoryLocator
+//					.getConnectionFactory(connectionData.getProviderId());
+//			resultList.add(connectionFactory.createConnection(connectionData));
+//		}
+//		return resultList;
+//	}
+	
+	private Connection<?> mapEntry(ConnectionData connectionData) {
+		ConnectionFactory<?> connectionFactory = connectionFactoryLocator.getConnectionFactory(connectionData.getProviderId());
+		return connectionFactory.createConnection(connectionData);
+	}
+	
+	private ConnectionData mapConnectionData(Node node) {
+		return new ConnectionData((String) node.getProperty("providerId"),
+				(String) node.getProperty("providerUserId"),
+				(String) node.getProperty("displayName"), 
+				(String) node.getProperty("profileUrl"),
+				(String) node.getProperty("imageUrl"),
+				decrypt((String) node.getProperty("accessToken")),
+				decrypt((String) node.getProperty("secret")),
+				decrypt((String) node.getProperty("refreshToken")),
+				expireTime((Long) node.getProperty("expireTime")));
+	}
+	
+	private List<Connection<?>> createResultList(ExecutionResult er) {
+		List<Connection<?>> resultList = new LinkedList<Connection<?>>();
+		Iterator<Node> userConnections = er.columnAs("connection");
+		while (userConnections.hasNext()) {
+			Node userConnectionNode = userConnections.next();
+			resultList.add(mapEntry(mapConnectionData(userConnectionNode)));
+		}
+		
+		
+		return resultList;
 	}
 
 }
